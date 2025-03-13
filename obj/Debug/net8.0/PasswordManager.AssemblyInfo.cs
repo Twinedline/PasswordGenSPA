7ad1b042bfb78d308270e347fb2f04eb10773ff2
Twinedline <Twinedline@gmail.com>
2025-03-13 14:48:41 +0000

    my password management system
    started the repository a bit late

--- obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/PasswordManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+065022c9e423423df3553e822c92af2ecec76a07")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
